481c81608f96971d0c27bfda9b601c8adc6dcd3e
Franz Bendezu <franzant20@gmail.com>
2021-11-19 11:42:56 -0500

    setup ts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,8 @@
 import 'tailwindcss/tailwind.css'
 
-function MyApp({ Component, pageProps }) {
+import type { AppProps } from 'next/app'
+
+function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
-
 export default MyApp
