cb43f5ff79061123d3bb3a8d3122656f8e4ca4cd
franz-bendezu <franzant20@gmail.com>
2023-03-15 13:54:19 +0000

    refactor: migrate to react 18 and next 13

--- components/layout/NavLink.tsx
+++ components/layout/NavLink.tsx
@@ -1,16 +1,17 @@
 import { useRouter } from "next/router";
 import Link from "next/link";
-import React, { PropsWithChildren } from "react";
+import React, { PropsWithChildren, ReactNode } from "react";
 
 export { NavLink };
 
-interface NavLinkProps{
+interface NavLinkProps {
   href: string;
   className?: string;
   activeClass?: string;
   exact?: boolean;
+  children: ReactNode
 }
-const defaultProps: NavLinkProps = {
+const defaultProps: Partial<NavLinkProps> = {
   exact: false,
   activeClass: "text-gray-800 font-bold dark:text-gray-400",
   href: "",
@@ -24,25 +25,23 @@ const NavLink: React.FC<NavLinkProps> = (props) => {
     isActive ? activeClass : "text-gray-600 dark:text-white font-normal "
   }`;
   return (
-    <Link href={href}>
-      <a className={currentClass} >
-        {children}{" "}
-        {isActive && (
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="16"
-            height="16"
-            fill="currentColor"
-            className="bi bi-arrow-down inline-block h-3 w-3"
-            viewBox="0 0 16 16"
-          >
-            <path
-              fillRule="evenodd"
-              d="M8 1a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L7.5 13.293V1.5A.5.5 0 0 1 8 1z"
-            />
-          </svg>
-        )}
-      </a>
+    <Link href={href} className={currentClass}>
+      {children}{" "}
+      {isActive && (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          fill="currentColor"
+          className="bi bi-arrow-down inline-block h-3 w-3"
+          viewBox="0 0 16 16"
+        >
+          <path
+            fillRule="evenodd"
+            d="M8 1a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L7.5 13.293V1.5A.5.5 0 0 1 8 1z"
+          />
+        </svg>
+      )}
     </Link>
   );
 };
--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -35,14 +35,13 @@ const Navigation: React.FC = () => {
   return (
     <nav className="sticky top-0 z-20 py-2 bg-white md:py-6  dark:bg-black">
       <div className="container px-4 mx-auto lg:max-w-4xl flex items-center justify-between">
-        <Link href="/">
-          <a
-            className={
-              "font-medium tracking-wider transition-colors text-gray-900 hover:text-sky-500 uppercase dark:text-white"
-            }
-          >
-            Franz Bendezu
-          </a>
+        <Link
+          href="/"
+          className={
+            "font-medium tracking-wider transition-colors text-gray-900 hover:text-sky-500 uppercase dark:text-white"
+          }
+        >
+          Franz Bendezu
         </Link>
         <div className=" flex items-center ">
           <Menu as={Fragment}>
