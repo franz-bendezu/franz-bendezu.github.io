25638d4f2a05c3f0ebe58888754004feb663ce13
franz-bendezu <franzant20@gmail.com>
2023-03-15 14:20:23 +0000

    fix: remove config for images

--- next.config.js
+++ next.config.js
@@ -1,9 +1,4 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  images: {
-    domains: ["franzbendezu.me"],
-    loader: "imgix",
-    path: "https://franzbendezu.me/",
-  },
 };
