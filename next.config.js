f255f1815e7b9db2626beace6c641c85ee5b1490
franz-bendezu <franzant20@gmail.com>
2023-03-15 14:36:21 +0000

    feat: setup config for images

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  images: {
+    formats: ['image/avif', 'image/webp'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'franzbendezu.me',
+        port: '',
+        pathname: '/image/upload/**',
+      },
+    ],
+  },
 };
