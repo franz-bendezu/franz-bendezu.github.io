6ebf6c583f5652a7c669caa4f09454c524a60a7a
Franz Bendezu <63468778+franz-bendezu@users.noreply.github.com>
2023-07-30 06:54:18 +0000

    fix: remove head title to app

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,9 @@
 import { NextPage } from "next";
-import Head from "next/head";
 import Presentation from "../components/home/Presentation";
 
 const Home: NextPage = () => {
   return (
     <div className="space-y-14 lg:space-y-24 ">
-      <Head>
-        <title>
-          Franz Bendezu - Software Developer
-        </title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
       <div className="max-w-4xl mx-auto antialiased">
         <Presentation />
       </div>
