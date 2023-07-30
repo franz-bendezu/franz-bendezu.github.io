d8eb4b49e1cde8dcf8e56f260140bee54923225a
Franz Bendezu <63468778+franz-bendezu@users.noreply.github.com>
2023-07-30 06:13:34 +0000

    fix: change title and remove  padding top

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,11 +6,13 @@ const Home: NextPage = () => {
   return (
     <div className="space-y-14 lg:space-y-24 ">
       <Head>
-        <title>Create Next App</title>
+        <title>
+          Franz Bendezu - Software Developer
+        </title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <div className="max-w-4xl mx-auto pt-16 antialiased">
+      <div className="max-w-4xl mx-auto antialiased">
         <Presentation />
       </div>
     </div>
