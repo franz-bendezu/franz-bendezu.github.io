0af0c154e951f01a313be0ee476f9a12b93b0ffa
Franz Bendezu <63468778+franz-bendezu@users.noreply.github.com>
2023-07-30 07:00:34 +0000

    fix: move container to _app

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,11 +3,9 @@ import Presentation from "../components/home/Presentation";
 
 const Home: NextPage = () => {
   return (
-    <div className="space-y-14 lg:space-y-24 ">
-      <div className="max-w-4xl mx-auto antialiased">
-        <Presentation />
-      </div>
-    </div>
+
+    <Presentation />
+
   );
 };
 
