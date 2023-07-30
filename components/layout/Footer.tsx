43da1eaca12162994fdedfb3380ef4fb8593fae2
Franz Bendezu <63468778+franz-bendezu@users.noreply.github.com>
2023-07-30 06:15:56 +0000

    refactor: organize presentation on home folder

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -2,35 +2,15 @@ import React from "react";
 
 const Footer: React.FC = () => {
   return (
-    <footer className=" px-6 container mx-auto text-gray-800 dark:text-white bg-white py-6 dark:bg-black">
-      <div className="pb-8 mb-2 border-t-2 border-gray-300 dark:border-white-300"></div>
-      <div className="flex flex-col justify-between lg:flex-row items-center">
-        <p>Diseñado por Franz Bendezu</p>
-        <div className="flex flex-wrap pt-2 sm:space-x-4 space-x-2 font-medium lg:pt-0">
-          <a
-            href="https://twitter.com/franz_bendezu"
-            className={"transition-colors hover:text-yellow-500"}
-            target="_blank"
-            rel="noreferrer"
-          >
-            Twitter
-          </a>
-          <a
-            href="https://www.linkedin.com/in/franz-antony-bendezu-isidro/"
-            className={"transition-colors hover:text-yellow-500"}
-            target="_blank"
-            rel="noreferrer"
-          >
-            LinkedIn
-          </a>
-          <a
-            href="https://github.com/franz-bendezu"
-            className={"transition-colors hover:text-yellow-500"}
-            target="_blank"
-            rel="noreferrer"
-          >
-            GitHub
-          </a>
+    <footer className="px-6 py-10 bg-white dark:bg-black text-gray-800 dark:text-white">
+      <div className="container mx-auto">
+        <div className="flex flex-col justify-between lg:flex-row items-center">
+          <p className="mb-6 lg:mb-0">Diseñado por Franz Bendezu</p>
+          <div className="flex flex-wrap space-x-4 font-medium">
+            <a href="https://twitter.com/franz_bendezu" className="transition-colors hover:text-yellow-500" target="_blank" rel="noreferrer">Twitter</a>
+            <a href="https://www.linkedin.com/in/franz-antony-bendezu-isidro/" className="transition-colors hover:text-yellow-500" target="_blank" rel="noreferrer">LinkedIn</a>
+            <a href="https://github.com/franz-bendezu" className="transition-colors hover:text-yellow-500" target="_blank" rel="noreferrer">GitHub</a>
+          </div>
         </div>
       </div>
     </footer>
