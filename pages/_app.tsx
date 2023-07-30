beb07aa1c9d75650e389134c01214ff15b30215a
Franz Bendezu <63468778+franz-bendezu@users.noreply.github.com>
2023-07-30 06:54:18 +0000

    fix: remove head title to app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import "tailwindcss/tailwind.css";
+import Head from "next/head";
 import Navbar from "../components/layout/Navbar";
 import Footer from "../components/layout/Footer";
 import type { AppProps } from "next/app";
@@ -8,12 +9,18 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <ThemeProvider attribute="class" defaultTheme="light">
-      <div className="  bg-white dark:bg-black flex flex-col min-h-screen">
-        <Navbar />
-        <main className=" flex-grow bg-white dark:bg-gray-900 justify-center text-center align-middle" >
-          <Component {...pageProps} />
-        </main>
-        <Footer />
+        <Head>
+          <title>
+            Franz Bendezu - Software Developer
+          </title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <div className="  bg-white dark:bg-black flex flex-col min-h-screen">
+          <Navbar />
+          <main className=" flex-grow bg-white dark:bg-gray-900 justify-center text-center align-middle" >
+            <Component {...pageProps} />
+          </main>
+          <Footer />
         </div>
       </ThemeProvider>
     </>
