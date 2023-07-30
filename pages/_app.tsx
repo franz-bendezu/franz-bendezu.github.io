4535db1206839ef95560df503e6b534a049dfc21
Franz Bendezu <63468778+franz-bendezu@users.noreply.github.com>
2023-07-30 07:01:53 +0000

    style: improvee code distribution

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,29 +7,22 @@ import { ThemeProvider } from "next-themes";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
-      <ThemeProvider attribute="class" defaultTheme="light">
-        <Head>
-          <title>
-            Franz Bendezu - Software Developer
-          </title>
-          <link rel="icon" href="/favicon.ico" />
-        </Head>
-        <div className="  bg-white dark:bg-black flex flex-col min-h-screen">
-          <Navbar />
-          <main className=" flex-grow bg-white dark:bg-gray-900 justify-center text-center align-middle" >
-            <div className="space-y-14 lg:space-y-24 ">
-              <div className="container mx-auto px-4 py-8">
-                <div className="max-w-4xl mx-auto antialiased">
-                  <Component {...pageProps} />
-                </div>
-              </div>
-            </div>
-          </main>
-          <Footer />
-        </div>
-      </ThemeProvider>
-    </>
+    <ThemeProvider attribute="class" defaultTheme="light">
+      <Head>
+        <title>Franz Bendezu - Software Developer</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="bg-white dark:bg-black flex flex-col min-h-screen">
+        <Navbar />
+        <main className="flex-grow bg-white dark:bg-gray-900 justify-center text-center align-middle">
+          <div className="space-y-14 lg:space-y-24 container mx-auto px-4 py-8 antialiased">
+            <Component {...pageProps} />
+          </div>
+        </main>
+        <Footer />
+      </div>
+    </ThemeProvider>
   );
 }
+
 export default MyApp;
