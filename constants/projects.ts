84c7a5f97c1dfa1e40a898283f122aa622275586
Franz Bendezu <63468778+franz-bendezu@users.noreply.github.com>
2024-04-27 23:24:23 +0000

    feat: add projects.ts to use PROJECT_CATEGORIES and PROJECTS constants

--- constants/projects.ts
+++ constants/projects.ts
@@ -1,35 +1,65 @@
-export const PROJECTS = [
-    {
-        status: "active",
-        title: "Project 1",
-        description: "This is a project description",
-        tags:['Vue 3', 'CSS', 'Typescript'],
-        image: {
-            src: "/images/project-1.jpg",
-            alt: "Project 1"
-        },
-        link: "https://github.com/franz-bendezu/franz-bendezu.github.io"
+import type {
+  IProject,
+  IProjectCategory,
+  ProjectCategoryCodes,
+} from "../interfaces/project";
+
+export const PROJECT_CATEGORIES: IProjectCategory<
+  ProjectCategoryCodes | "all"
+>[] = [
+  {
+    name: "Todos",
+    value: "all",
+  },
+  {
+    name: "Freelance",
+    value: "freelance",
+  },
+  {
+    name: "Personal",
+    value: "personal",
+  },
+  {
+    name: "Demo",
+    value: "demo",
+  },
+];
+
+export const PROJECTS: IProject[] = [
+  {
+    status: "active",
+    title: "Project 1",
+    description: "This is a project description",
+    tags: ["Vue 3", "CSS", "Typescript"],
+    image: {
+      src: "/images/project-1.jpg",
+      alt: "Project 1",
     },
-    {
-        status: "completed",
-        title: "Project 2",
-        description: "This is a project description",
-        tags:['React', 'Tailwind CSS', 'Javascript'],
-        image: {
-            src: "/images/project-2.jpg",
-            alt: "Project 2"
-        },
-        link: "https://github.com/franz-bendezu/franz-bendezu.github.io"
+    link: "https://github.com/franz-bendezu/franz-bendezu.github.io",
+    categoryCode: "freelance",
+  },
+  {
+    status: "completed",
+    title: "Project 2",
+    description: "This is a project description",
+    tags: ["React", "Tailwind CSS", "Javascript"],
+    image: {
+      src: "/images/project-2.jpg",
+      alt: "Project 2",
     },
-    {
-        status: "completed",
-        title: "Project 3",
-        description: "This is a project description",
-        tags:['Next.js', 'SASS', 'Javascript'],
-        image: {
-            src: "/images/project-3.jpg",
-            alt: "Project 3"
-        },
-        link: "https://github.com/franz-bendezu/franz-bendezu.github.io"
+    link: "https://github.com/franz-bendezu/franz-bendezu.github.io",
+    categoryCode: "personal",
+  },
+  {
+    status: "completed",
+    title: "Project 3",
+    description: "This is a project description",
+    tags: ["Next.js", "SASS", "Javascript"],
+    image: {
+      src: "/images/project-3.jpg",
+      alt: "Project 3",
     },
-]
+    link: "https://github.com/franz-bendezu/franz-bendezu.github.io",
+    categoryCode: "demo",
+  },
+];
