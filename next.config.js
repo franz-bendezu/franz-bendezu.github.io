4f111df7e73b71cad550aa4e5fdaea6d71ae8a27
Franz Bendezu <63468778+franz-bendezu@users.noreply.github.com>
2024-04-27 15:34:18 +0000

    feat: Add i18n configuration to next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   output: "export",
+  i18n: {
+    locales: ["en-US", "es-ES"],
+    defaultLocale: "es-ES",
+  },
   reactStrictMode: true,
   images: {
     formats: ["image/avif", "image/webp"],
