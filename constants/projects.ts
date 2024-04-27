c914a5afad715b3e51f5f71ada8446a053ba662b
Franz Bendezu <63468778+franz-bendezu@users.noreply.github.com>
2024-04-27 05:10:55 +0000

    feat: add projects.ts file with project data

---
+++ constants/projects.ts
@@ -0,0 +1,35 @@
+export const PROJECTS = [
+    {
+        status: "active",
+        title: "Project 1",
+        description: "This is a project description",
+        tags:['Vue 3', 'CSS', 'Typescript'],
+        image: {
+            src: "/images/project-1.jpg",
+            alt: "Project 1"
+        },
+        link: "https://github.com/franz-bendezu/franz-bendezu.github.io"
+    },
+    {
+        status: "completed",
+        title: "Project 2",
+        description: "This is a project description",
+        tags:['React', 'Tailwind CSS', 'Javascript'],
+        image: {
+            src: "/images/project-2.jpg",
+            alt: "Project 2"
+        },
+        link: "https://github.com/franz-bendezu/franz-bendezu.github.io"
+    },
+    {
+        status: "completed",
+        title: "Project 3",
+        description: "This is a project description",
+        tags:['Next.js', 'SASS', 'Javascript'],
+        image: {
+            src: "/images/project-3.jpg",
+            alt: "Project 3"
+        },
+        link: "https://github.com/franz-bendezu/franz-bendezu.github.io"
+    },
+]
