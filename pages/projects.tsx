259d05654a5b13262ce498f6c0fca85d1e984c97
Franz Bendezu <63468778+franz-bendezu@users.noreply.github.com>
2024-04-27 05:11:19 +0000

    feat: update Projects page with Card component and project data

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,6 +1,41 @@
 import { NextPage } from "next";
+import Card from "../components/ui/Card";
+import { PROJECTS } from "../constants/projects";
+import { ArrowRightIcon } from "@heroicons/react/20/solid";
 
 const Projects: NextPage = () => {
-  return <div></div>;
+  return (
+    <section
+      data-testid="projects"
+      className="my-5 flex flex-col items-center justify-center"
+    >
+      <h1 className="font-monospace text-3xl dark:text-white lg:text-4xl">
+        Proyectos
+      </h1>
+      <span className="mb-5 h-1 w-32 bg-blue-500"></span>
+      <div className="mb-10 flex w-full flex-col flex-wrap items-center md:flex-row">
+        {PROJECTS.map((project) => (
+          <Card
+            key={project.title}
+            url={project.link}
+            title={project.title}
+            description={project.description}
+            image={project.image}
+            tags={project.tags}
+          />
+        ))}
+      </div>
+      <a
+        href="https://github.com/"
+        target="_blank"
+        rel="noopener"
+        className="flex flex-row text-black hover:text-blue-500 dark:text-white dark:hover:text-blue-500"
+        aria-label="link to repository"
+      >
+        Ver más en GitHub
+        <ArrowRightIcon className="ml-1 h-6 w-6"></ArrowRightIcon>
+      </a>
+    </section>
+  );
 };
 export default Projects;
