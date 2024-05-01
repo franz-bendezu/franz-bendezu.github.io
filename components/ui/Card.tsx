e84960da4fb9d4957c0d2404dbbf5f7afc70b268
Franz Bendezu <63468778+franz-bendezu@users.noreply.github.com>
2024-05-01 04:31:31 +0000

    feat: add dataTestId prop to Card component in components/ui/Card.tsx

--- components/ui/Card.tsx
+++ components/ui/Card.tsx
@@ -1,13 +1,14 @@
 interface CardProps {
   className?: string;
   children?: React.ReactNode;
+  dataTestId?: string;
 }
 
 const Card: React.FC<CardProps> = (props) => {
   const { className, children } = props;
   return (
     <article
-      data-testId="card"
+      data-testid={props.dataTestId}
       className={
         className +
         ` rounded-xl p-3 shadow-lg transition-all duration-100 ease-in-out hover:scale-105
