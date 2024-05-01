6415c51b411a438b7512f321312f4383726ec3f4
Franz Bendezu <63468778+franz-bendezu@users.noreply.github.com>
2024-05-01 04:45:37 +0000

    feat: add NavbarDesktop component to add activeClass prop in NavLink

--- components/layout/NavbarDesktop.tsx
+++ components/layout/NavbarDesktop.tsx
@@ -8,7 +8,12 @@ const NavbarDesktop: React.FC<{
   return (
     <div className="hidden md:ml-10 md:block md:space-x-8 md:pr-4">
       {routes.map((route) => (
-        <NavLink href={route.path} key={route.title} exact={route.exact}>
+        <NavLink
+          href={route.path}
+          key={route.title}
+          exact={route.exact}
+          activeClass=" font-bold dark:white bg-gray-300 dark:bg-gray-800 rounded-md"
+        >
           {route.title}
         </NavLink>
       ))}
