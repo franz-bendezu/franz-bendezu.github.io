34c217d27b58c0da76669e251770db9ee57bf21f
Franz Bendezu <63468778+franz-bendezu@users.noreply.github.com>
2024-05-04 23:11:21 +0000

    refactor: Organize project constants into a nested directory structure

---
+++ constants/projects/techologies.ts
@@ -0,0 +1,116 @@
+import {
+  IProjectTechnology,
+  IProjectTechonologyCategory,
+} from "../../interfaces/project";
+
+export const PROJECT_TECHNOLOGY_CATEGORY = [
+  {
+    name: "Cloud",
+    value: "cloud",
+  },
+  {
+    name: "Database",
+    value: "database",
+  },
+  {
+    name: "DevOps",
+    value: "devops",
+  },
+  {
+    name: "Testing",
+    value: "testing",
+  },
+  {
+    name: "Frontend Framework",
+    value: "frontend-framework",
+  },
+  {
+    name: "Backend Framework",
+    value: "backend-framework",
+  },
+  {
+    name: "Fullstack Framework",
+    value: "fullstack-framework",
+  },
+  {
+    name: "Web API",
+    value: "web-api",
+  },
+  {
+    name: "Programming Language",
+    value: "programming-language",
+  },
+  {
+    name: "IoT",
+    value: "iot",
+  },
+] as const satisfies IProjectTechonologyCategory[];
+
+export const PROJECT_TECHNOLOGIES: IProjectTechnology[] = [
+  {
+    code: "nuxtjs",
+    name: "Nuxt.js",
+    color: "#00C58E",
+    categoryCodes: ["frontend-framework"],
+  },
+  {
+    code: "typescript",
+    name: "Typescript",
+    color: "#3178C6",
+    categoryCodes: ["programming-language"],
+  },
+  {
+    code: "graphql",
+    name: "GraphQL",
+    color: "#E535AB",
+    categoryCodes: ["web-api"],
+  },
+  {
+    code: "apollo-server",
+    name: "Apollo Server",
+    color: "#3B2E5A",
+    categoryCodes: ["backend-framework"],
+  },
+  {
+    code: "firebase",
+    name: "Firebase",
+    color: "#FFCA28",
+    categoryCodes: ["cloud"],
+  },
+  {
+    code: "vue2",
+    name: "Vue 2",
+    color: "#4FC08D",
+    categoryCodes: ["frontend-framework"],
+  },
+  {
+    code: "cloud-functions",
+    name: "Cloud Functions",
+    color: "#FFD600",
+    categoryCodes: ["devops"],
+  },
+  {
+    code: "iot",
+    name: "LoT",
+    color: "#FFD600",
+    categoryCodes: ["devops"],
+  },
+  {
+    code: "lorawan",
+    name: "LoRaWAN",
+    color: "#FFD600",
+    categoryCodes: ["devops"],
+  },
+  {
+    code: "spring-boot",
+    name: "Spring Boot",
+    color: "#6DB33F",
+    categoryCodes: ["backend-framework"],
+  },
+  {
+    code: "java",
+    name: "Java",
+    color: "#007396",
+    categoryCodes: ["programming-language"],
+  },
+];
