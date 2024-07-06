8f31be4371d21236a578c3d0494edde19fe7e43e
Franz Bendezu <63468778+franz-bendezu@users.noreply.github.com>
2024-07-06 23:54:11 +0000

    fix: change typo for vue2

--- constants/projects/index.ts
+++ constants/projects/index.ts
@@ -63,7 +63,8 @@ export const PROJECTS: IBaseProject[] = [
   {
     status: "completed",
     title: "Movify.pe - Módulo de Pagos con Seguimiento de Ingresos y Gastos",
-    shortDescription: "Desarrollo de un módulo de pagos con seguimiento de ingresos y gastos.",
+    shortDescription:
+      "Desarrollo de un módulo de pagos con seguimiento de ingresos y gastos.",
     description: `De julio a diciembre de 2023, desarrollé un módulo de pagos para facilitar el registro de pagos y auditorías con recibos para órdenes de mudanza dentro de la infraestructura existente de la empresa. Implementé funciones para registrar transacciones de pago y asociarlas con las órdenes de mudanza respectivas, asegurando una documentación financiera precisa y rastros de auditoría. Mejoré las capacidades de informes para generar reportes detallados de los registros de pago y recibos asociados, facilitando los procesos de auditoría y análisis financiero.`,
     tags: ["AWS S3", "Java", "MariaDB", "Spring Boot", "Vue.js"],
     image: {
@@ -71,12 +72,13 @@ export const PROJECTS: IBaseProject[] = [
       alt: "Módulo de Pagos",
     },
     categoryCode: "freelance",
-    technologyCodes: ["aws-s3", "java", "mariadb", "spring-boot", "vue"],
+    technologyCodes: ["aws-s3", "java", "mariadb", "spring-boot", "vue2"],
   },
   {
     status: "completed",
     title: "Movify.pe - Sistema de Cotizaciones Web (Migración)",
-    shortDescription: "Migración del sistema de cotizaciones web a una API serverless.",
+    shortDescription:
+      "Migración del sistema de cotizaciones web a una API serverless.",
     description: `De julio a diciembre de 2023, migré el backend del sistema de cotizaciones web a una API serverless, mejorando la disponibilidad y reduciendo los costos de servidor. Mejoré la compatibilidad del diseño de correos electrónicos con varios proveedores, lo que resultó en un aumento de la generación de clientes potenciales. Integré análisis para rastrear las vistas de correos electrónicos, proporcionando información valiosa sobre las tasas de apertura y el compromiso.`,
     tags: ["Node.js", "Prisma", "AWS Lambda", "GitHub Actions", "MJML"],
     image: {
@@ -84,33 +86,75 @@ export const PROJECTS: IBaseProject[] = [
       alt: "Sistema de Cotizaciones",
     },
     categoryCode: "freelance",
-    technologyCodes: ["nodejs", "prisma", "aws-lambda", "github-actions", "mjml"],
+    technologyCodes: [
+      "nodejs",
+      "prisma",
+      "aws-lambda",
+      "github-actions",
+      "mjml",
+    ],
   },
   {
     status: "completed",
     title: "Movify.pe - Migración de Infraestructura a AWS",
-    shortDescription: "Migración de infraestructura a AWS para mejorar el despliegue y la disponibilidad.",
+    shortDescription:
+      "Migración de infraestructura a AWS para mejorar el despliegue y la disponibilidad.",
     description: `De julio a noviembre de 2023, lideré la migración de la infraestructura a AWS para mejorar el despliegue, la disponibilidad y la confiabilidad de los servicios clave. Establecí flujos de trabajo de CI/CD para la implementación en servicios de AWS, automatizando las tuberías. Integré con Amazon S3 para el almacenamiento de fotos y videos relacionados con los servicios de mudanza.`,
-    tags: ["Amplify", "Amazon RDS", "EC2", "S3", "Spring Framework", "Java", "Vue.js"],
+    tags: [
+      "Amplify",
+      "Amazon RDS",
+      "EC2",
+      "S3",
+      "Spring Framework",
+      "Java",
+      "Vue.js",
+    ],
     image: {
       src: "/images/aws-migration.jpg",
       alt: "Migración a AWS",
     },
     categoryCode: "freelance",
-    technologyCodes: ["aws-amplify", "amazon-rds", "aws-ec2", "aws-s3", "spring-framework", "java", "vue"],
+    technologyCodes: [
+      "aws-amplify",
+      "amazon-rds",
+      "aws-ec2",
+      "aws-s3",
+      "spring-framework",
+      "java",
+      "vue2",
+    ],
   },
   {
     status: "completed",
     title: "Movify.pe - Sistema de Gestión de Cotizaciones y Órdenes",
-    shortDescription: "Desarrollo de un sistema administrativo para cotizaciones y órdenes de mudanza.",
+    shortDescription:
+      "Desarrollo de un sistema administrativo para cotizaciones y órdenes de mudanza.",
     description: `De diciembre de 2021 a julio de 2023, desarrollé un sistema administrativo para cotizaciones y órdenes para el equipo de ventas y mudanza. Diseñé e implementé la arquitectura cliente-servidor utilizando una API REST y una SPA. Automatizé la generación de PDF para cotizaciones y órdenes de venta. Desarrollé un módulo de informes para medir la eficiencia del equipo de ventas e interconecté con el sistema de cotizaciones web a través de API. Establecí flujos de integración y despliegue continuo mediante GitHub Actions.`,
-    tags: ["Spring Boot", "JPA Hibernate", "Java", "TypeScript", "Vue.js", "Vuetify", "Nginx", "Docker"],
+    tags: [
+      "Spring Boot",
+      "JPA Hibernate",
+      "Java",
+      "TypeScript",
+      "Vue.js",
+      "Vuetify",
+      "Nginx",
+      "Docker",
+    ],
     image: {
       src: "/images/order-management.jpg",
       alt: "Sistema de Gestión de Cotizaciones y Órdenes",
     },
     categoryCode: "freelance",
-    technologyCodes: ["spring-boot", "jpa-hibernate", "java", "typescript", "vue", "vuetify", "nginx", "docker"],
+    technologyCodes: [
+      "spring-boot",
+      "jpa-hibernate",
+      "java",
+      "typescript",
+      "vue2",
+      "vuetify",
+      "nginx",
+      "docker",
+    ],
   },
   {
     status: "completed",
@@ -126,4 +170,3 @@ export const PROJECTS: IBaseProject[] = [
     technologyCodes: ["react", "javascript", "html", "css", "wordpress"],
   },
 ];
-
