fc9e63482a0568acd617daf8e70e460f91ba80ed
Franz Bendezu <63468778+franz-bendezu@users.noreply.github.com>
2024-07-07 21:02:56 +0000

    refactor: add vitest configuration and test for about page

---
+++ pages/__tests__/about.test.tsx
@@ -0,0 +1,10 @@
+import { expect, test } from "vitest";
+import { render, screen } from "@testing-library/react";
+import Page from "../about";
+
+test("Page", () => {
+  render(<Page />);
+  expect(
+    screen.getByRole("heading", { level: 1, name: "Acerca de mí" }),
+  ).toBeDefined();
+});
