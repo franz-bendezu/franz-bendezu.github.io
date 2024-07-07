5893e2f4ff8656c42261f1f4c41dda0a35e1e8d2
Franz Bendezu <63468778+franz-bendezu@users.noreply.github.com>
2024-07-07 20:49:09 +0000

    refactor: Add target and rel attributes to project link in LinkList component

--- components/project/LinkList.tsx
+++ components/project/LinkList.tsx
@@ -25,6 +25,8 @@ export const LinkListItem: FC<{ link: IProjectLink }> = ({ link }) => {
       key={link.url}
       href={link.url}
       aria-label={link.title}
+      target="_blank"
+      rel="noopener noreferrer"
       className="inline-flex items-center justify-center whitespace-nowrap rounded-lg border border-blue-500 bg-blue-500 px-3 py-1.5 text-sm font-medium text-white transition-colors duration-150 hover:bg-blue-600 focus-visible:outline-none focus-visible:ring focus-visible:ring-blue-300 focus-visible:ring-indigo-300 dark:border-slate-700 dark:bg-slate-800 dark:text-slate-300 dark:hover:bg-slate-900 dark:focus-visible:ring-slate-600"
     >
       <Icon className="mr-1.5 h-5 w-5" />
