0fd73e8c4b6a45f02601b4fb8ef3efe6650db221
Franz Bendezu <63468778+franz-bendezu@users.noreply.github.com>
2024-07-07 00:34:35 +0000

    fix: improvee content of project distribution

--- components/project/Card.tsx
+++ components/project/Card.tsx
@@ -21,7 +21,7 @@ const ProjectCard: React.FC<ProjectCardProps> = (props) => {
     <section className="w-full p-4 md:w-1/2 lg:w-1/3">
       <Card
         data-testId="card"
-        className="bg-white dark:bg-gray-800 dark:text-white  h-full "
+        className="h-full bg-white dark:bg-gray-800 dark:text-white flex justify-between flex-col"
       >
         <a href={url} aria-label="link to project">
           <div className="relative flex items-end overflow-hidden rounded-xl">
@@ -36,15 +36,14 @@ const ProjectCard: React.FC<ProjectCardProps> = (props) => {
           </div>
           <div className="mt-1 p-2">
             <h3 className="text-xl text-blue-500 lg:text-2xl">{title}</h3>
-            <p
-              className="text-sm text-black dark:text-white lg:text-base
-            "
-            >
+            <p className="text-sm text-black dark:text-white lg:text-base">
               {description}
             </p>
-            <TagList tags={technologies} />
           </div>
         </a>
+        <div className="mt-1 p-2">
+          <TagList tags={technologies} />
+        </div>
       </Card>
     </section>
   );
