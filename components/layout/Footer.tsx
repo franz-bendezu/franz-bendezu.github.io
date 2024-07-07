62d986e77bfe341f6670d9297d2cfc14563f57ea
Franz Bendezu <63468778+franz-bendezu@users.noreply.github.com>
2024-07-07 11:04:12 +0000

    refactor: update Footer component to include social media icons

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -1,34 +1,40 @@
 import React from "react";
+import TwitterIcon from "../icon/twitter";
+import GitHubIcon from "../icon/github";
+import LinkedInIcon from "../icon/linkedin";
 
 const Footer: React.FC = () => {
   return (
-    <footer className="px-6 py-10 text-gray-800  dark:text-white">
+    <footer className="px-6 py-10 text-gray-800 dark:text-white">
       <div className="container mx-auto">
         <div className="flex flex-col items-center justify-between lg:flex-row">
           <p className="mb-6 lg:mb-0">Diseñado por Franz Bendezu</p>
           <div className="flex flex-wrap space-x-4 font-medium">
             <a
               href="https://twitter.com/franz_bendezu"
-              className="transition-colors hover:text-yellow-500"
+              className="flex items-center gap-3 transition-colors hover:text-yellow-500"
               target="_blank"
               rel="noreferrer"
             >
+              <TwitterIcon className="h-5 w-5" />
               Twitter
             </a>
             <a
               href="https://www.linkedin.com/in/franz-antony-bendezu-isidro/"
-              className="transition-colors hover:text-yellow-500"
+              className="flex items-center gap-3 transition-colors hover:text-yellow-500"
               target="_blank"
               rel="noreferrer"
             >
+              <LinkedInIcon className="h-5 w-5" />
               LinkedIn
             </a>
             <a
               href="https://github.com/franz-bendezu"
-              className="transition-colors hover:text-yellow-500"
+              className="flex items-center gap-3 transition-colors hover:text-yellow-500"
               target="_blank"
               rel="noreferrer"
             >
+              <GitHubIcon className="h-5 w-5" />
               GitHub
             </a>
           </div>
