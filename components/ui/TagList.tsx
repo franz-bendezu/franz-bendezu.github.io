c51bede5911223c496f5afaca7f746b8e5455149
Franz Bendezu <63468778+franz-bendezu@users.noreply.github.com>
2024-07-07 11:29:00 +0000

    refactor: set TechStack component to handle click events on technology items

--- components/ui/TagList.tsx
+++ components/ui/TagList.tsx
@@ -1,29 +1,42 @@
 import Image from "next/image";
 import { IProjectTechnology } from "../../interfaces/project";
 
-const TechStackItem: React.FC<{ tech: IProjectTechnology }> = ({ tech: tag }) => {
+const TechStackItem: React.FC<{
+  tech: IProjectTechnology;
+  onClick: () => void;
+}> = ({ tech: tag, onClick }) => {
   return (
-    <div className="flex flex-row gap-2 rounded px-2 py-1 text-xs outline outline-1 lg:text-sm">
+    <button
+      className="flex flex-row gap-2 rounded px-2 py-1 text-xs outline outline-1 lg:text-sm"
+      onClick={onClick}
+    >
       {tag.logo && (
         <Image
           src={tag.logo}
           alt={""}
-          className="h-4 w-4 "
+          className="h-4 w-4"
           width="16"
           height="16"
         />
       )}
       {tag.name}
-    </div>
+    </button>
   );
 };
 
-const TechStack: React.FC<{ techs: IProjectTechnology[] }> = ({ techs: tags }) => {
+const TechStack: React.FC<{
+  techs: IProjectTechnology[];
+  onClickTech: (tech: IProjectTechnology) => void;
+}> = ({ techs: tags, onClickTech }) => {
   return (
     tags && (
       <div className="mt-2 flex flex-row flex-wrap justify-between gap-2">
         {tags.map((tag) => (
-          <TechStackItem key={tag.code} tech={tag} />
+          <TechStackItem
+            key={tag.code}
+            tech={tag}
+            onClick={() => onClickTech(tag)}
+          />
         ))}
       </div>
     )
