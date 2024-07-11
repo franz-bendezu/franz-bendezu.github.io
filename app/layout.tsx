d269d7bd517ca582a56bf81f9380febbcc75bf17
Franz Bendezu <63468778+franz-bendezu@users.noreply.github.com>
2024-07-11 14:21:19 +0000

    refactor: add metadata to RootLayout component for SEO

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,13 @@
 import "tailwindcss/tailwind.css";
 import { ReactNode } from "react";
+const isProd = process.env.NODE_ENV === 'production'
 
 type Props = {
   children: ReactNode[];
 };
-
+export const metadata = {
+  metadataBase: isProd ? 'https://franzbendezu.com' : undefined,
+}
 export default function RootLayout({ children}: Props) {
   return  children 
 }
