9458f67145c558d9399a12684f5478810f4a86ee
Franz Bendezu <63468778+franz-bendezu@users.noreply.github.com>
2024-07-11 05:54:35 +0000

    chore: Update getStaticPaths in [category].tsx to support multiple locales

--- pages/projects/c/[category].tsx
+++ pages/projects/c/[category].tsx
@@ -2,11 +2,15 @@ import { GetStaticPaths } from "next";
 import { PROJECT_CATEGORIES } from "../../../constants/projects";
 import ProjectsPage, { getStaticProps } from "..";
 
-export const getStaticPaths = (async () => {
+export const getStaticPaths = (async ({ locales = [] }) => {
+  const paths: { params: { category: string }; locale: string }[] = [];
+  for (const category of PROJECT_CATEGORIES) {
+    for (const locale of locales) {
+      paths.push({ params: { category: category.value }, locale });
+    }
+  }
   return {
-    paths: PROJECT_CATEGORIES.map((category) => ({
-      params: { category: category.value },
-    })),
+    paths: paths,
     fallback: false, // false or "blocking"
   };
 }) satisfies GetStaticPaths;
