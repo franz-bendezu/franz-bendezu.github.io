c5ca68eb2c2ef687cfc7622eb677e9dde3affc69
Franz Bendezu <63468778+franz-bendezu@users.noreply.github.com>
2024-07-11 12:41:41 +0000

    refactor: update Navbar component to include locale prop

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -24,9 +24,10 @@ const Navigation: React.FC<{ locale: string }> = ({ locale }) => {
 
   return (
     <nav className="sticky top-0 z-20 border-b-[1px] border-slate-700/90 bg-white !bg-opacity-30 py-2 backdrop-blur-lg backdrop-filter dark:bg-gray-900 md:py-6">
-      <div className="container mx-auto flex items-center justify-between px-4 lg:max-w-4xl">
+      <div className="container mx-auto flex items-center justify-between px-4 ">
         <Link
           href="/"
+          locale={locale}
           className={
             "font-medium uppercase tracking-wider text-gray-900 transition-colors hover:text-sky-500 dark:text-white"
           }
