933de51cf4dadd51431f34a8e62d68e4ce6f0ea5
Franz Bendezu <63468778+franz-bendezu@users.noreply.github.com>
2024-07-11 04:15:16 +0000

    chore: Update styling for NavLink and Navbar components

--- components/ui/ThemeSwtich.tsx
+++ components/ui/ThemeSwtich.tsx
@@ -13,7 +13,7 @@ const ThemeSwitch = () => {
     <button
       aria-label="Toggle Dark Mode"
       type="button"
-      className="ml-1 mr-1 h-8 w-8 rounded p-1 sm:ml-4"
+      className=" h-8 w-8 rounded p-1"
       onClick={() =>
         setTheme(
           theme === "dark" || resolvedTheme === "dark" ? "light" : "dark",
