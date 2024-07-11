c0831c9eae7b78089393edfd6df5c06e7b164369
Franz Bendezu <63468778+franz-bendezu@users.noreply.github.com>
2024-07-11 15:19:25 +0000

    refactor: Update RootLayout component with metadata for SEO and i18n support

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,21 @@
 import "tailwindcss/tailwind.css";
 import { ReactNode } from "react";
+import { Metadata } from "next";
 const isProd = process.env.NODE_ENV === 'production'
 
 type Props = {
   children: ReactNode[];
 };
-export const metadata = {
-  metadataBase: isProd ? 'https://franzbendezu.me' : undefined,
+
+export const metadata:Metadata = {
+  metadataBase: isProd ? new URL('https://franzbendezu.me') : undefined,
+  alternates: {
+    canonical: '/',
+    languages: {
+      'es': '/es',
+      'en': '/en',
+    },
+  },
 }
 export default function RootLayout({ children}: Props) {
   return  children 
