4167ed144c8ec2aa87ddb5fce481d316789f7c70
Franz Bendezu <63468778+franz-bendezu@users.noreply.github.com>
2024-07-11 15:21:41 +0000

    refactor: set RootLayout component description for SEO and i18n support

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -5,10 +5,12 @@ import { Metadata } from "next";
 type Props = {
   children: ReactNode;
 };
+
 export const metadata: Metadata = {
   title: "Franz Bendezu - Desarrollador de Software",
-  description: "Franz Bendezu - Desarrollador de Software",
+  description: "Bienvenido a mi sitio web personal",
 };
+
 export default function RootLayout({ children }: Props) {
   return <_RootLayout params={{ locale: "en" }}>{children}</_RootLayout>;
 }
