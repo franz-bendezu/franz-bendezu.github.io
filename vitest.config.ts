b2c2af04154d07c757ec085bb76ca585adbb326e
Franz Bendezu <63468778+franz-bendezu@users.noreply.github.com>
2024-07-12 04:55:33 +0000

    refactor: add ProjectsPage test

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
+import path from 'path'
  
 export default defineConfig({
   plugins: [react()],
@@ -8,4 +9,9 @@ export default defineConfig({
     environment: "jsdom",
     setupFiles: ["./setupTests.ts"],
   },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './'),
+    },
+  }
 })
