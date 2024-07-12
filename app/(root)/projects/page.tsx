e2d9a3c14c18c9c8d03bea5481d8193812847a62
Franz Bendezu <63468778+franz-bendezu@users.noreply.github.com>
2024-07-12 05:37:22 +0000

    refactor: use resolver import paths

--- app/(root)/projects/page.tsx
+++ app/(root)/projects/page.tsx
@@ -1,10 +1,8 @@
 import { Metadata } from "next";
-
-export { default } from "../../[locale]/projects/page";
+export { default } from "@/app/[locale]/projects/page";
 
 export const metadata: Metadata = {
-    title: {
-      absolute: "Proyectos",
-    },
-  };
-  
+  title: {
+    absolute: "Proyectos",
+  },
+};
