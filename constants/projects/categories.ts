db159ad52efd9e73a256f0e516d3c9b16bf07e10
Franz Bendezu <63468778+franz-bendezu@users.noreply.github.com>
2024-07-18 04:54:12 +0000

    refactor: update project categories to separate file

---
+++ constants/projects/categories.ts
@@ -0,0 +1,16 @@
+import { IProjectCategory } from "@/interfaces/project";
+
+export const PROJECT_CATEGORIES: IProjectCategory[] = [
+    {
+      name: "Freelance",
+      value: "freelance",
+    },
+    {
+      name: "Personal",
+      value: "personal",
+    },
+    {
+      name: "Demo",
+      value: "demo",
+    },
+  ];
--- constants/projects/index.ts
+++ constants/projects/index.ts
@@ -1,19 +1,5 @@
-import type { IBaseProject, IProjectCategory } from "../../interfaces/project";
-
-export const PROJECT_CATEGORIES: IProjectCategory[] = [
-  {
-    name: "Freelance",
-    value: "freelance",
-  },
-  {
-    name: "Personal",
-    value: "personal",
-  },
-  {
-    name: "Demo",
-    value: "demo",
-  },
-];
+import type { IBaseProject } from "../../interfaces/project";
+export { PROJECT_CATEGORIES } from "./categories";
 
 export const PROJECTS: IBaseProject[] = [
   {
