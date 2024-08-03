872832bcc05af6eb40ac1d8113e023d5422c208c
Franz Bendezu <63468778+franz-bendezu@users.noreply.github.com>
2024-08-03 18:05:13 +0000

    refactor: add ExperienceEducationCard component for displaying education experience

--- src/app/[locale]/about/page.tsx
+++ src/app/[locale]/about/page.tsx
@@ -3,7 +3,6 @@ import {
   WORK_EXPERIENCES,
   EDUCATION_EXPERIENCES,
 } from "../../../constants/experiences";
-import Card from "../../../components/ui/Card";
 import {
   Bars4Icon,
   BriefcaseIcon,
@@ -13,6 +12,7 @@ import { getTranslations, unstable_setRequestLocale } from "next-intl/server";
 import { useTranslations } from "next-intl";
 import { DEFAULT_LOCALE } from "@/constants/locales";
 import { ExperienceWorkCard } from "@/components/experience/WorkCard";
+import { ExperienceEducationCard } from "@/components/experience/EducationCard";
 
 type Props = {
   params: { locale?: string };
@@ -47,28 +47,9 @@ const AboutPage: NextPage<Props> = ({
           Educación
         </h2>
         <div className="container">
-          <div className="flex grid-cols-12 flex-col text-gray-50 md:grid">
+          <div className="flex grid-cols-12 flex-col  md:grid">
             {EDUCATION_EXPERIENCES.map((p) => (
-              <div key={p.id} className="flex md:contents">
-                <div className="relative col-start-2 col-end-4 mr-10 md:mx-auto">
-                  <div className="flex h-full w-6 items-center justify-center">
-                    <div className="pointer-events-none h-full w-1 bg-blue-500"></div>
-                  </div>
-                  <div className="absolute top-1/2 -mt-3 h-6 w-6 rounded-full bg-blue-500 text-center shadow">
-                    <i className="fas fa-check-circle text-white"></i>
-                  </div>
-                </div>
-                <Card className="col-start-4 col-end-12 my-4 mr-auto w-full bg-blue-500 px-4 py-3">
-                  <h3 className="mb-1 text-lg font-semibold">{p.degree}</h3>
-                  <a
-                    href={p.link ?? ""}
-                    className={`font-semibold ${p?.link ? "text-primary" : "text-foreground"}`}
-                  >
-                    {p.institution}
-                  </a>
-                  <div className="mb-4 text-sm uppercase">{p.location}</div>
-                </Card>
-              </div>
+              <ExperienceEducationCard key={p.id} {...p} />
             ))}
           </div>
         </div>
