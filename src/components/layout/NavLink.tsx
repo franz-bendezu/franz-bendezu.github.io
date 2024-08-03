0909130567a6dc796b84ef205a438f2aeb8515ae
Franz Bendezu <63468778+franz-bendezu@users.noreply.github.com>
2024-08-03 22:12:02 +0000

    refactor: add DownArrowIcon component for improved navigation

--- src/components/layout/NavLink.tsx
+++ src/components/layout/NavLink.tsx
@@ -1,6 +1,7 @@
 "use client";
 import React, { MouseEvent, ReactNode, forwardRef } from "react";
 import { Link, usePathname } from "@/navigation";
+import { DownArrowIcon } from "../icon/DownIcon";
 
 interface NavLinkProps {
   href: string;
@@ -11,22 +12,6 @@ interface NavLinkProps {
   locale?: string;
   onClick?: (event: MouseEvent<HTMLAnchorElement>) => void;
 }
-
-const DownArrowIcon = () => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width="16"
-    height="16"
-    fill="currentColor"
-    className="bi bi-arrow-down inline-block h-3 w-3"
-    viewBox="0 0 16 16"
-  >
-    <path
-      fillRule="evenodd"
-      d="M8 1a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L7.5 13.293V1.5A.5.5 0 0 1 8 1z"
-    />
-  </svg>
-);
 
 const NavLink: React.FC<NavLinkProps> = forwardRef(
   function NavLink(props, ref) {
