bab0a2073674e56c964c8d0ff7a45ff85fc4b327
Franz Bendezu <63468778+franz-bendezu@users.noreply.github.com>
2024-08-03 17:55:04 +0000

    refactor: add WorkExperienceCard component for displaying work experience

--- src/app/[locale]/about/page.tsx
+++ src/app/[locale]/about/page.tsx
@@ -12,6 +12,7 @@ import {
 import { getTranslations, unstable_setRequestLocale } from "next-intl/server";
 import { useTranslations } from "next-intl";
 import { DEFAULT_LOCALE } from "@/constants/locales";
+import { WorkExperienceCard } from "@/components/work/Card";
 
 type Props = {
   params: { locale?: string };
@@ -75,37 +76,9 @@ const AboutPage: NextPage<Props> = ({
           <BriefcaseIcon className="h-8 w-8" /> Experiencia Laboral
         </h2>
         <div className="container">
-          <div className="flex grid-cols-12 flex-col text-gray-50 md:grid">
+          <div className="flex grid-cols-12 flex-col md:grid">
             {WORK_EXPERIENCES.map((p) => (
-              <div key={p.id} className="flex md:contents">
-                <div className="relative col-start-2 col-end-4 mr-10 md:mx-auto">
-                  <div className="flex h-full w-6 items-center justify-center">
-                    <div className="pointer-events-none h-full w-1 bg-blue-500"></div>
-                  </div>
-                  <div className="absolute top-1/2 -mt-3 h-6 w-6 rounded-full bg-blue-500 text-center shadow">
-                    <i className="fas fa-check-circle text-white"></i>
-                  </div>
-                </div>
-                <Card className="col-start-4 col-end-12 my-4 mr-auto w-full bg-blue-500 px-4 py-3">
-                  <h3 className="mb-1 text-lg font-semibold">{p.position}</h3>
-                  <a
-                    href={p.link ?? ""}
-                    className={`font-semibold ${p?.link ? "text-primary" : "text-foreground"}`}
-                  >
-                    {p.company}
-                  </a>
-                  <div className="mb-4 text-sm uppercase">
-                    {p.start} - {p.end}
-                  </div>
-                  <div className="pl-3 text-justify leading-tight">
-                    <ul className="prose dark:prose-invert list-disc text-sm">
-                      {p.tasks.map((i) => (
-                        <li key={i}>{i}</li>
-                      ))}
-                    </ul>
-                  </div>
-                </Card>
-              </div>
+              <WorkExperienceCard key={p.id} {...p} />
             ))}
           </div>
         </div>
