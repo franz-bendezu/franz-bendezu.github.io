da9e63ca6b70ff382c52cb5f4242f3c7be545494
Franz Bendezu <63468778+franz-bendezu@users.noreply.github.com>
2024-08-03 18:31:47 +0000

    feat: improve ExperienceWorkCard component layout and styling for range

--- src/components/experience/WorkCard.tsx
+++ src/components/experience/WorkCard.tsx
@@ -10,12 +10,18 @@ export const ExperienceWorkCard: FC<Props> = (props) => {
       <h3 className="mb-1 text-lg font-semibold">{props.position}</h3>
       <a
         href={props.link ?? ""}
-        className={`font-semibold ${props?.link ? "text-primary" : "text-foreground"}`}
+        className={`font-semibold pb-2 ${props?.link ? "text-primary" : "text-foreground"}`}
       >
         {props.company}
       </a>
-      <div className="mb-4 text-sm uppercase">
-        {props.start} - {props.end}
+      <div className="my-2 text-sm uppercase flex items-center gap-2">
+        <div className="rounded-full bg-blue-600/80 px-2 py-1 text-xs font-semibold text-white">
+          {props.start}
+        </div>
+        -
+        <div className="rounded-full bg-blue-600/80  px-2 py-1 text-xs font-semibold text-white">
+          {props.end}
+        </div>
       </div>
       <div className="pl-3 text-justify leading-tight">
         <ul className="prose dark:prose-invert list-disc text-sm">
