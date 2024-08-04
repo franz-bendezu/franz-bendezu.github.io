4d833e3f2d59c66062562030f27bebd73aab3039
Franz Bendezu <63468778+franz-bendezu@users.noreply.github.com>
2024-08-04 23:14:00 +0000

    refactor: Update LastProjects component to display localized title and description

--- src/components/home/LastProjects.tsx
+++ src/components/home/LastProjects.tsx
@@ -1,17 +1,18 @@
+import { useTranslations } from "next-intl";
 import { ProjectList } from "../project/List";
 
 export default function LastProjects() {
+  const t = useTranslations("Home.lastProjects");
   return (
     <section id="projects" className="w-full py-6">
       <div className="container mx-auto px-4">
         <div className="flex flex-col items-center justify-center space-y-4 text-center">
           <div className="space-y-2">
             <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">
-              My Latest Projects
+              {t("title")}
             </h2>
             <p className="text-muted-foreground max-w-[900px] md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
-              Explore a selection of my recent web development projects,
-              showcasing my skills and creativity.
+              {t("description")}
             </p>
           </div>
         </div>
