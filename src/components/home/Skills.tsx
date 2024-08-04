48b1b17e30a79472c8e91bc340d8dd0751bde94f
Franz Bendezu <63468778+franz-bendezu@users.noreply.github.com>
2024-08-04 22:33:24 +0000

    refactor: Update Skills component with updated technology stack

--- src/components/home/Skills.tsx
+++ src/components/home/Skills.tsx
@@ -1,10 +1,10 @@
 export const Skills = () => {
   return (
-    <section id="skills" className="bg-gray-300 w-full py-6">
+    <section id="skills" className="w-full bg-gray-300 py-6">
       <div className="container mx-auto px-4">
         <div className="flex flex-col items-center justify-center space-y-4 text-center">
           <div className="space-y-2">
-            <div className="bg-gray-100 inline-block rounded-lg px-3 py-1 text-sm">
+            <div className="inline-block rounded-lg bg-gray-100 px-3 py-1 text-sm">
               My Skills
             </div>
             <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">
@@ -21,19 +21,20 @@ export const Skills = () => {
             <div className="grid gap-1">
               <h3 className="text-xl font-bold">Front-end</h3>
               <p className="text-muted-foreground">
-                React, Next.js, JavaScript, TypeScript, HTML, CSS, Tailwind CSS
+                Vue, Nuxt, Angular, React, Next.js, Tailwind CSS, Vuetify,
+                Bootstrap, Material UI, Angular Material
               </p>
             </div>
             <div className="grid gap-1">
               <h3 className="text-xl font-bold">Back-end</h3>
               <p className="text-muted-foreground">
-                Node.js, Express, Python, Django, Ruby on Rails
+                Spring Framework, Node.js, Express, Nestjs
               </p>
             </div>
             <div className="grid gap-1">
-              <h3 className="text-xl font-bold">Cloud</h3>
+              <h3 className="text-xl font-bold">Cloud/ DevOps</h3>
               <p className="text-muted-foreground">
-                AWS, Google Cloud, Vercel, Netlify, Heroku
+                AWS, DigitalOcean, Docker, GitHub Actions, GitLab CI/CD
               </p>
             </div>
           </div>
@@ -41,19 +42,19 @@ export const Skills = () => {
             <div className="grid gap-1">
               <h3 className="text-xl font-bold">Databases</h3>
               <p className="text-muted-foreground">
-                PostgreSQL, MySQL, MongoDB, Firebase
+                PostgreSQL, MySQL, MariaDB, MongoDB
               </p>
             </div>
             <div className="grid gap-1">
               <h3 className="text-xl font-bold">Tools</h3>
               <p className="text-muted-foreground">
-                Git, GitHub, Figma, Jira, Trello, VSCode
+                Git, GitHub, Gitlab, Figma, Jira, Trello, VSCode
               </p>
             </div>
             <div className="grid gap-1">
               <h3 className="text-xl font-bold">Testing</h3>
               <p className="text-muted-foreground">
-                Jest, Cypress, Enzyme, React Testing Library
+                Vitest, Jest, Karma, Jasmine, Cypress, Junit, Mockito
               </p>
             </div>
           </div>
