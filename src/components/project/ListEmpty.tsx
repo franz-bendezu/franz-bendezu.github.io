4e26a9543ca5785c8d7a3b57bbc28309a67e0853
Franz Bendezu <63468778+franz-bendezu@users.noreply.github.com>
2024-08-04 23:14:00 +0000

    refactor: Update LastProjects component to display localized title and description

--- src/components/project/ListEmpty.tsx
+++ src/components/project/ListEmpty.tsx
@@ -1,6 +1,7 @@
 import { ExclamationTriangleIcon } from "@heroicons/react/16/solid";
+import { FC } from "react";
 
-export const ProjectListEmpty: React.FC = () => (
+export const ProjectListEmpty: FC = () => (
   <div className="flex w-full flex-col items-center justify-center gap-4">
     <ExclamationTriangleIcon className="h-12 w-12 text-yellow-500 dark:text-yellow-500" />
     <p className="text-2xl dark:text-white">No hay proyectos para mostrar</p>
