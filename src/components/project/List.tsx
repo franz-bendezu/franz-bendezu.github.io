bfbc304da36fdee4db00757005aad9ec8bafc6fb
Franz Bendezu <63468778+franz-bendezu@users.noreply.github.com>
2024-08-04 22:41:26 +0000

    refactor: rename ProjectList component to ProjectExplorerList and update references

---
+++ src/components/project/List.tsx
@@ -0,0 +1,40 @@
+import { FC } from "react";
+import ProjectCard from "./Card";
+import { ProjectListEmpty } from "./ListEmpty";
+import { IProject, IProjectTechnology } from "@/interfaces/project";
+
+type Props = {
+  projects: IProject[];
+  onClickProjectTech: (project: IProject, tech: IProjectTechnology) => void;
+  selectedTechs: IProjectTechnology[];
+};
+
+export const ProjectList: FC<Props> = ({
+  projects,
+  selectedTechs,
+  onClickProjectTech: handleClickTech,
+}) => {
+  return (
+    <div className="flex w-full flex-col flex-wrap items-stretch md:flex-row">
+      {projects.length > 0 ? (
+        projects.map((project) => (
+          <ProjectCard
+            key={project.title}
+            code={project.code}
+            links={project.links}
+            title={project.title}
+            description={project.shortDescription}
+            image={project.image}
+            technologies={project.technologies}
+            selectedTechs={selectedTechs}
+            onClickTech={
+                (tech: IProjectTechnology) => handleClickTech(project, tech)
+            }
+          />
+        ))
+      ) : (
+        <ProjectListEmpty />
+      )}
+    </div>
+  );
+};
