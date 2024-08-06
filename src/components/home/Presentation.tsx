af42964cdf476610bd466bb9eabdbaa5dc1cace4
Franz Bendezu <63468778+franz-bendezu@users.noreply.github.com>
2024-08-06 11:54:20 +0000

    refactor: update LinkedIn profile link in HomePresentation component

--- src/components/home/Presentation.tsx
+++ src/components/home/Presentation.tsx
@@ -44,7 +44,7 @@ const HomePresentation: FC = () => {
           </div>
           <div className="flex flex-row gap-2 flex-wrap justify-center lg:justify-start">
             <a
-              href="https://www.linkedin.com/in/franz-antony-bendezu-isidro/"
+              href="https://www.linkedin.com/in/franz-bendez/"
               className="flex items-center gap-3 rounded-md px-3 py-1 transition-colors hover:bg-gray-300 dark:text-white dark:hover:bg-gray-700"
               target="_blank"
               rel="noreferrer"
