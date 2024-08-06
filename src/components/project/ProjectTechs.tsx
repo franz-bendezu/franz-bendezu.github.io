381a2b4c1d0aade69b3f370877878965dce58951
Franz Bendezu <63468778+franz-bendezu@users.noreply.github.com>
2024-08-06 16:27:44 +0000

    refactor: extract components for project page

---
+++ src/components/project/ProjectTechs.tsx
@@ -0,0 +1,37 @@
+import { IBaseProjectTechnology } from "@/interfaces/project";
+import { useTranslations } from "next-intl";
+import { FC } from "react";
+import Image from "next/image";
+
+export const ProjectTechs: FC<{ techs: IBaseProjectTechnology[] }> = ({
+    techs,
+  }) => {
+    const t = useTranslations("Project");
+    return (
+      <section>
+        <h2 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl">
+          {t("technologies")}
+        </h2>
+        <div className="mt-6 grid grid-cols-2 gap-4">
+          {techs.map((tech) => (
+            <div
+              key={tech.code}
+              className="rounded-lg bg-slate-200 p-4 text-center dark:bg-slate-700"
+            >
+              {tech?.logo && (
+                <Image
+                  src={tech.logo}
+                  alt={""}
+                  className="mx-auto h-8 w-8"
+                  width="32"
+                  height="32"
+                />
+              )}
+              <p className="mt-2 text-sm font-medium">{tech.name}</p>
+            </div>
+          ))}
+        </div>
+      </section>
+    );
+  };
+  
---
+++ src/components/project/Summary.tsx
@@ -0,0 +1,47 @@
+import { IBaseProject } from "@/interfaces/project";
+import { FC } from "react";
+import Image from "next/image";
+
+export const ProjectSummary: FC<{
+    project: IBaseProject;
+  }> = ({ project }) => {
+    return (
+      <section className="border-b">
+        <div className="grid gap-10 px-4 py-6 md:px-6 md:py-8 lg:grid-cols-2 lg:gap-20 lg:py-12">
+          <div className="order-2 space-y-4 lg:order-1">
+            <div className="inline-block rounded-lg bg-slate-200 px-3 py-1 text-sm dark:bg-slate-700">
+              {project.categoryCode}
+            </div>
+            <h1 className="text-4xl font-bold tracking-tighter sm:text-5xl md:text-6xl">
+              {project.title}
+            </h1>
+            <p className="max-w-[600px] md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
+              {project.shortDescription}
+            </p>
+            <div className="flex flex-col gap-2 min-[400px]:flex-row">
+              {project.links?.map((link) => (
+                <a
+                  key={link.url}
+                  href={link.url}
+                  className="inline-block rounded-md bg-slate-500 px-4 py-2 text-sm font-medium text-white hover:bg-slate-600 dark:bg-slate-700 dark:hover:bg-slate-800"
+                >
+                  {link.title}
+                </a>
+              ))}
+            </div>
+          </div>
+          {project.banner && (
+            <div className="relative order-1 flex items-center justify-center lg:order-2">
+              <Image
+                src={project.banner.src}
+                alt={project.banner.alt}
+                width="800"
+                height="450"
+                className="mx-auto aspect-video w-max overflow-hidden rounded-xl object-cover object-center"
+              />
+            </div>
+          )}
+        </div>
+      </section>
+    );
+  };
