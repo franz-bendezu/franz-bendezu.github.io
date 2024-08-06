5af08afedf965128d096c54653be9ead51eca8e2
Franz Bendezu <63468778+franz-bendezu@users.noreply.github.com>
2024-08-06 10:53:37 +0000

    feat: add Oximetro Perú project details

--- src/constants/projects/index.ts
+++ src/constants/projects/index.ts
@@ -122,6 +122,65 @@ export const PROJECTS: IBaseProject[] = [
   },
   {
     status: "completed",
+    title: "Oximetro Perú",
+    shortDescription:
+      "Desarrollo del MVP de una aplicación web para ver lugares donde encontrar oxímetros cerca de ti.",
+    description:
+      "En agosto de 2020, desarrollé un MVP de una aplicación web denominada Oximetro Perú, que permitía a los usuarios encontrar lugares cercanos donde comprar oxímetros. Utilicé Vue.js para el frontend y Firebase para el backend, lo que permitió una implementación rápida y eficiente. Además, integré Leaflet.js para la visualización de mapas y geolocalización, lo que facilitó a los usuarios encontrar tiendas cercanas. Este proyecto se desarrolló en respuesta a la creciente demanda de oxímetros durante la pandemia de COVID-19 en Perú.",
+    tags: ["Vue.js", "Firebase", "Leaflet.js"],
+    image: {
+      src: "/images/oximetro-peru/banner.webp",
+      alt: "Oximetro Perú",
+    },
+    links: [
+      {
+        title: "Demo",
+        url: "https://oximetro-peru.web.app/",
+        type: "website",
+      },
+    ],
+    categoryCode: "freelance",
+    technologyCodes: ["vue2", "firebase", "leaflet"],
+    start: "2020-05",
+    code: "oximetro-peru",
+    end: "2020-06",
+    projectGoals: [
+      {
+        title: "Localización de Oxímetros",
+        description:
+          "Desarrollar una aplicación para encontrar lugares cercanos donde alquilar oxímetros para la detección de COVID-19.",
+      },
+      {
+        title: "Respuesta a la Pandemia",
+        description:
+          "Responder a la creciente demanda de oxímetros durante la pandemia de COVID-19 en Perú.",
+      },
+      {
+        title: "Desarrollo Rápido y Eficiente",
+        description:
+          "Utilizar tecnologías modernas para un desarrollo rápido y eficiente del MVP.",
+      },
+    ],
+    keyFeatures: [
+      {
+        title: "Localización de Tiendas",
+        description:
+          "Visualización de tiendas cercanas donde alquilar oxímetros para la detección de COVID-19.",
+      },
+      {
+        title: "Geolocalización",
+        description:
+          "Integración de Leaflet.js para la visualización de mapas y geolocalización.",
+      },
+      {
+        title: "Desarrollo Ágil",
+        description:
+          "Desarrollo rápido y eficiente utilizando Vue.js y Firebase.",
+      },
+    ],
+  },
+  {
+    status: "completed",
     title: "Horext",
     shortDescription: "Proyecto personal para generar horarios académicos.",
     description:
