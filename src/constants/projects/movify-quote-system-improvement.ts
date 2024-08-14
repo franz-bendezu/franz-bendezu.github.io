5dbb7748c509ebd2f7fcaad22916096372cca7ec
Franz Bendezu <63468778+franz-bendezu@users.noreply.github.com>
2024-08-14 21:45:58 +0000

    feat: improve MOVIFY_QUOTE_SYSTEM_IMPROVEMENT project with new images for each step

--- src/constants/projects/movify-quote-system-improvement.ts
+++ src/constants/projects/movify-quote-system-improvement.ts
@@ -1,10 +1,11 @@
 import { IBaseProject } from "@/interfaces/project";
 
-export const MOVIFY_QUOTE_SYSTEM_IMPROVEMENT:IBaseProject  = {
+export const MOVIFY_QUOTE_SYSTEM_IMPROVEMENT: IBaseProject = {
   status: "completed",
   title: "Movify.pe - Mejora del Sistema de Cotizaciones Web",
   shortDescription: "Mejora del sistema de cotizaciones web.",
-  description: "De octubre a diciembre de 2021, mejoré la interfaz del sistema de cotizaciones web integrando un estilo personalizado para Google Maps y estableciendo una nueva UI/UX para cada paso de la cotización y la plantilla de correo electrónico. Migré un iframe a un plugin de WordPress para un mejor manejo de actualizaciones. Desarrollé funciones para la captura de clientes potenciales y datos de auditoría.",
+  description:
+    "De octubre a diciembre de 2021, mejoré la interfaz del sistema de cotizaciones web integrando un estilo personalizado para Google Maps y estableciendo una nueva UI/UX para cada paso de la cotización y la plantilla de correo electrónico. Migré un iframe a un plugin de WordPress para un mejor manejo de actualizaciones. Desarrollé funciones para la captura de clientes potenciales y datos de auditoría.",
   tags: ["React", "JavaScript", "HTML", "CSS", "WordPress"],
   categoryCode: "freelance",
   technologyCodes: [
@@ -26,29 +27,63 @@ export const MOVIFY_QUOTE_SYSTEM_IMPROVEMENT:IBaseProject  = {
   projectGoals: [
     {
       title: "Mejora de la Interfaz de Usuario",
-      description: "Integrar un estilo personalizado de Google Maps y mejorar la UI/UX para el proceso de cotización.",
+      description:
+        "Integrar un estilo personalizado de Google Maps y mejorar la UI/UX para el proceso de cotización.",
     },
     {
       title: "Mejorar el proceso de actualización",
-      description: "Migrar un iframe a un plugin de WordPress para un mejor manejo de actualizaciones.",
+      description:
+        "Migrar un iframe a un plugin de WordPress para un mejor manejo de actualizaciones.",
     },
     {
       title: "Captura de Clientes Potenciales",
-      description: "Desarrollar funciones para capturar clientes potenciales y datos de auditoría.",
+      description:
+        "Desarrollar funciones para capturar clientes potenciales y datos de auditoría.",
     },
   ],
   keyFeatures: [
     {
       title: "Estilo Personalizado de Google Maps",
-      description: "Integración de un estilo personalizado de Google Maps para una mejor experiencia de usuario.",
+      description:
+        "Integración de un estilo personalizado de Google Maps para una mejor experiencia de usuario.",
     },
     {
       title: "Migración a WordPress",
-      description: "Migración de un iframe a un plugin de WordPress para una mejor gestión de actualizaciones.",
+      description:
+        "Migración de un iframe a un plugin de WordPress para una mejor gestión de actualizaciones.",
     },
     {
       title: "Captura de Clientes Potenciales",
-      description: "Desarrollo de funciones para capturar clientes potenciales y datos de auditoría.",
+      description:
+        "Desarrollo de funciones para capturar clientes potenciales y datos de auditoría.",
+    },
+  ],
+  images: [
+    {
+      src: "/images/movify-quote-system-improvement/step-1.webp",
+      alt: "Step 1",
+      description: "Nuevo paso para llenar datos del cliente.",
+    },
+    {
+      src: "/images/movify-quote-system-improvement/step-2.webp",
+      alt: "Step 2",
+      description: "Nuevo paso para llenar datos de la mudanza.",
+    },
+    {
+      src: "/images/movify-quote-system-improvement/step-3.webp",
+      alt: "Step 3",
+      description: "Nuevo paso para llenar los objetos a mudar.",
+    },
+    {
+      src: "/images/movify-quote-system-improvement/step-4.webp",
+      alt: "Step 4",
+      description:
+        "Nuevo paso para mostrar el precio de las opciones de mudanza.",
+    },
+    {
+      src: "/images/movify-quote-system-improvement/step-5.webp",
+      alt: "Step 5",
+      description: "Nuevo paso para mostrar los servicios adicionales.",
     },
   ],
 };
--- src/constants/projects/movify-quote-system-migration.ts
+++ src/constants/projects/movify-quote-system-migration.ts
@@ -50,31 +50,4 @@ export const MOVIFY_QUOTE_SYSTEM_MIGRATION:IBaseProject  = {
       description: "Integración de análisis para rastrear las vistas de correos electrónicos y mejorar las tasas de apertura.",
     },
   ],
-  images: [
-    {
-      src: "/images/movify-quote-system-migration/step-1.webp",
-      alt: "Step 1",
-      description: "Nuevo paso para llenar datos del cliente.",
-    },
-    {
-      src: "/images/movify-quote-system-migration/step-2.webp",
-      alt: "Step 2",
-      description: "Nuevo paso para llenar datos de la mudanza.",
-    },
-    {
-      src: "/images/movify-quote-system-migration/step-3.webp",
-      alt: "Step 3",
-      description: "Nuevo paso para llenar los objetos a mudar.",
-    },
-    {
-      src: "/images/movify-quote-system-migration/step-4.webp",
-      alt: "Step 4",
-      description: "Nuevo paso para mostrar el precio de las opciones de mudanza.",
-    },
-    {
-      src: "/images/movify-quote-system-migration/step-5.webp",
-      alt: "Step 5",
-      description: "Nuevo paso para mostrar los servicios adicionales.",
-    },
-  ]
 };
