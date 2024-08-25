f6f2a331bbb6b68ea272bfd897f03614b4d4812e
Franz Bendezu <63468778+franz-bendezu@users.noreply.github.com>
2024-08-25 03:49:48 +0000

    refactor: update image dimensions in HomePresentation component

--- src/components/home/Presentation.tsx
+++ src/components/home/Presentation.tsx
@@ -11,10 +11,10 @@ const HomePresentation: FC = () => {
   const t = useTranslations("Home");
 
   return (
-    <section id="presentation" className="w-full py-6 relative">
-      <div className="absolute z-1 bg-gradient-to-r from-gray-300 to-gray-200 dark:from-slate-800 dark:to-slate-700" />
-      <div className="container z-2 mx-auto flex flex-col-reverse items-center gap-3 px-4 text-center lg:flex lg:flex-row lg:space-x-5 lg:text-left">
-        <div className="flex flex-col items-center space-y-4 lg:items-start max-w-full ">
+    <section id="presentation" className="relative w-full py-6">
+      <div className="z-1 absolute bg-gradient-to-r from-gray-300 to-gray-200 dark:from-slate-800 dark:to-slate-700" />
+      <div className="z-2 container mx-auto flex flex-col-reverse items-center gap-3 px-4 text-center lg:flex lg:flex-row lg:space-x-5 lg:text-left">
+        <div className="flex max-w-full flex-col items-center space-y-4 lg:items-start">
           <div className="flex flex-col items-center gap-3 lg:items-start">
             <h1 className="text-md font-semibold">{t("greeting")}</h1>
             <h1 className="text-4xl font-semibold">{t("name")}</h1>
@@ -42,7 +42,7 @@ const HomePresentation: FC = () => {
               })}
             </p>
           </div>
-          <div className="flex flex-row gap-2 flex-wrap justify-center lg:justify-start">
+          <div className="flex flex-row flex-wrap justify-center gap-2 lg:justify-start">
             <a
               href="https://www.linkedin.com/in/franz-bendez/"
               className="flex items-center gap-3 rounded-md px-3 py-1 transition-colors hover:bg-gray-300 dark:text-white dark:hover:bg-gray-700"
@@ -75,7 +75,7 @@ const HomePresentation: FC = () => {
             <HomePresentationDownload />
           </div>
         </div>
-        <div className="flex flex-shrink-0 items-center justify-center max-w-full">
+        <div className="flex max-w-full flex-shrink-0 items-center justify-center">
           <div className="relative flex aspect-square h-auto max-h-[512px] w-[512px] max-w-[512px] items-center justify-center">
             <CircularPulse
               className="aspect-square h-auto max-w-[100%]"
@@ -83,8 +83,8 @@ const HomePresentation: FC = () => {
               height="512"
             />
             <Image
-              src={"/profile.webp"}
-              blurDataURL="/profile.webp"
+              src="/images/profile.webp"
+              blurDataURL="/images/profile.webp"
               alt="Profile"
               fetchPriority={"high"}
               className="ma-auto relative z-10 max-h-[50%] max-w-[50%] rounded-full object-cover"
