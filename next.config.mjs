0b83d53aa50ba3208747eca8a6eed4111a94004c
Franz Bendezu <63468778+franz-bendezu@users.noreply.github.com>
2024-08-25 03:45:18 +0000

    refactor: update image dimensions in ProjectImagesCarousel and HomePresentation components

--- next.config.mjs
+++ next.config.mjs
@@ -7,7 +7,7 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     loader: "custom",
-    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384, 640, 750, 828],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
   },
   transpilePackages: ["next-image-export-optimizer"],
