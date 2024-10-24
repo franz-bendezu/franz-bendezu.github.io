4a8e1093adc139e747773e5812d35d48390495ab
Franz Bendezu <63468778+franz-bendezu@users.noreply.github.com>
2024-10-24 11:51:19 +0000

    fix: support async render of react components

--- src/__tests__/about.test.tsx
+++ src/__tests__/about.test.tsx
@@ -2,17 +2,21 @@ import { expect, test, vi } from "vitest";
 import { render, screen } from "@testing-library/react";
 import AboutPage from "../app/[locale]/about/page";
 import { NextIntlClientProvider } from "next-intl";
+import { act } from "react";
 
 vi.mock("next-intl/server", () => ({
   unstable_setRequestLocale: vi.fn(),
 }));
 
-test("Page", () => {
-  render(
-    <NextIntlClientProvider locale="en">
-      <AboutPage params={{ locale: "en" }} />
-    </NextIntlClientProvider>,
-  );
+test("Page", async () => {
+  await act(async () => {
+    render(
+      <NextIntlClientProvider locale="en">
+        <AboutPage params={Promise.resolve({ locale: "en" })} />
+      </NextIntlClientProvider>,
+    );
+  });
+
   expect(
     screen.getByRole("heading", { level: 1, name: "About.title" }),
   ).toBeDefined();
--- src/__tests__/projects.test.tsx
+++ src/__tests__/projects.test.tsx
@@ -2,18 +2,20 @@ import { expect, test, vi } from "vitest";
 import { render, screen } from "@testing-library/react";
 import ProjectsPage from "../app/[locale]/projects/page";
 import { NextIntlClientProvider } from "next-intl";
+import { act } from "react";
 
 vi.mock("next-intl/server", () => ({
   unstable_setRequestLocale: vi.fn(),
 }));
 
-test("ProjectsPage", () => {
-  render(
-    <NextIntlClientProvider locale="en">
-      <ProjectsPage params={{ locale: "en" }} />
-    </NextIntlClientProvider>,
-  );
-
+test("ProjectsPage", async () => {
+  await act(async () => {
+    render(
+      <NextIntlClientProvider locale="en">
+        <ProjectsPage params={Promise.resolve({ locale: "en" })} />
+      </NextIntlClientProvider>,
+    );
+  });
   expect(
     screen.getByRole("heading", { level: 1, name: "Projects.title" }),
   ).toBeDefined();
