cffb7714cc1c6e29b59fa2ad2cf567e28849c8df
Franz Bendezu <63468778+franz-bendezu@users.noreply.github.com>
2025-01-23 01:01:51 -0500

    refactor: use only top 3 in home page.tsx

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,15 +1,25 @@
-import { PROJECTS } from "@/constants/projects";
+import { use } from "react";
+import { DEFAULT_LOCALE } from "@/constants/locales";
+import HomePresentation from "../../components/home/Presentation";
+import { unstable_setRequestLocale } from "next-intl/server";
+import { Skills } from "@/components/home/Skills";
 import { LastProjects } from "@/components/home/LastProjects";
+import { getDataByCategory } from "./projects/page";
 
-const HomePage = () => {
-  // Sort projects by position and take the top 3
-  const topProjects = PROJECTS.sort((a, b) => a.position - b.position).slice(0, 3);
+export default function LocaleRootPage({
+  params,
+}: {
+  params: Promise<{ locale: string }>;
+}) {
+  const { locale = DEFAULT_LOCALE } = use(params);
 
+  unstable_setRequestLocale(locale);
+  const projects = getDataByCategory()..sort((a, b) => a.position - b.position).slice(0, 3);
   return (
-    <div>
-      <LastProjects projects={topProjects} />
-    </div>
+    <>
+      <HomePresentation />
+      <Skills></Skills>
+      <LastProjects projects={projects}></LastProjects>
+    </>
   );
-};
-
-export default HomePage;
+}
