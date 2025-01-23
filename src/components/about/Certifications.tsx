6592f23522caf0c1ecbc010056e6054e6d7d126d
Franz Bendezu <63468778+franz-bendezu@users.noreply.github.com>
2025-01-23 00:02:56 -0500

    fix: remove title on Certifications.tsx

--- src/components/about/Certifications.tsx
+++ src/components/about/Certifications.tsx
@@ -4,8 +4,6 @@ import Card from "../ui/Card";
 
 const Certifications = () => {
   return (
-    <div>
-      <h2 className="text-2xl font-bold mb-4">Certifications</h2>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         {COURSE_CERTIFICATIONS.map((certification) => (
           <Card key={certification.id} className="bg-white dark:bg-gray-800 p-4">
@@ -22,7 +20,6 @@ const Certifications = () => {
           </Card>
         ))}
       </div>
-    </div>
   );
 };
 
