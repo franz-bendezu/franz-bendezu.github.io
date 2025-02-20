4cce13c7840ea54f9b2ce99a6b411ae11dfaedc0
Franz Bendezu <63468778+franz-bendezu@users.noreply.github.com>
2025-02-20 10:22:21 -0500

    fix: change syntax for import global css in layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import Footer from "./globals.css";
+import "./globals.css";
 import { ReactNode } from "react";
 import { Metadata } from "next";
 const isProd = process.env.NODE_ENV === "production";
