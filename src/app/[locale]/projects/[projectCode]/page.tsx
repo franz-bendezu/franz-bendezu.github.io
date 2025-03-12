e21cf7f5cbca13383e244357d3ee5fc8a22bc93e
Franz Bendezu <63468778+franz-bendezu@users.noreply.github.com>
2025-03-12 00:26:51 -0500

    fix: support find project by locale

--- src/app/[locale]/projects/[projectCode]/page.tsx
+++ src/app/[locale]/projects/[projectCode]/page.tsx
@@ -1,51 +1,61 @@
-import { notFound } from "next/navigation";
+import { use } from "react";
+import { DEFAULT_LOCALE } from "@/constants/locales";
 import { PROJECTS } from "@/constants/projects";
-import { IBaseProject } from "@/interfaces/project";
-import { Project } from "@/components/project/Project";
-import { Metadata } from "next";
-import { getDictionary } from "@/utils/get-dictionary";
-import { Locale } from "@/i18n-config";
+import { PROJECT_TECHNOLOGIES } from "@/constants/project-techologies";
+import { useTranslations } from "next-intl";
+import { unstable_setRequestLocale } from "next-intl/server";
+import { ProjectSummary } from "@/components/project/Summary";
+import { ProjectOverview } from "@/components/project/Overview";
+import { ProjectGoals } from "@/components/project/Goals";
+import { ProjectTechsList } from "@/components/project/TechsList";
+import { ProjectFeatures } from "@/components/project/Features";
+import { ProjectImagesCarousel } from "@/components/project/Images";
 
-interface Props {
-  params: {
-    projectCode: string;
-    locale: Locale;
-  };
-}
-
-export async function generateMetadata({
-  params,
-}: Props): Promise<Metadata | undefined> {
-  const { projectCode, locale } = params;
-  const dict = await getDictionary(locale);
-
-  const project = PROJECTS.find(
-    (project) => project.code === projectCode && project.lang === locale
-  );
-
-  if (!project) {
-    return;
+export const generateStaticParams = () => {
+  const params: { projectCode: string }[] = [];
+  for (const project of PROJECTS) {
+    params.push({ projectCode: project.code });
   }
+  return params;
+};
 
-  return {
-    title: {
-      default: project.title,
-      template: `%s | ${dict.Metadata.title.template}`,
-    },
-    description: project.shortDescription,
-  };
-}
+type Props = {
+  params: Promise<{
+    projectCode: string;
+    locale?: string;
+  }>;
+};
 
 export default function ProjectPage({ params }: Props) {
-  const { projectCode, locale } = params;
-
-  const project = PROJECTS.find(
-    (project) => project.code === projectCode && project.lang === locale
+  const { locale = DEFAULT_LOCALE, projectCode } = use(params);
+  unstable_setRequestLocale(locale);
+  const project = PROJECTS.find((project) => project.code === projectCode);
+  const techs = PROJECT_TECHNOLOGIES.filter((tech) =>
+    project?.technologyCodes.includes(tech.code) && project.lang === locale,
   );
-
+  const t = useTranslations("Project");
   if (!project) {
-    notFound();
+    return <div>Project not found</div>;
   }
+  return (
+    <div className="container flex min-h-[100dvh] flex-col gap-8 md:gap-8 lg:gap-16">
+      <ProjectSummary project={project} />
 
-  return <Project project={project as IBaseProject} />;
+      {project.description && (
+        <ProjectOverview description={project.description} />
+      )}
+      <div>
+        <div className="grid gap-10 px-4 md:px-6 lg:grid-cols-2 lg:gap-20">
+          <ProjectTechsList techs={techs} />
+          <ProjectGoals goals={project.projectGoals} />
+        </div>
+      </div>
+      <div>
+        <div className="grid gap-10 px-4 md:px-6 lg:grid-cols-2 lg:gap-20">
+          <ProjectFeatures project={project} />
+          {project.images && <ProjectImagesCarousel images={project.images} />}
+        </div>
+      </div>
+    </div>
+  );
 }
