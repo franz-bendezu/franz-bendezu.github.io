10c61801ecfe6b1f52adb491d8f37ccbaa4d409d
Franz Bendezu <63468778+franz-bendezu@users.noreply.github.com>
2025-03-12 00:23:22 -0500

    fix: support filter projects by lang param passing locale.tsx

--- src/app/[locale]/projects/page.tsx
+++ src/app/[locale]/projects/page.tsx
@@ -1,51 +1,111 @@
-import { notFound } from "next/navigation";
-import { PROJECTS } from "@/constants/projects";
-import { IBaseProject } from "@/interfaces/project";
-import { Project } from "@/components/project/Project";
+import { use } from "react";
+import { PROJECTS, PROJECT_CATEGORIES } from "../../../constants/projects";
+import { CodeBracketIcon } from "@heroicons/react/20/solid";
+import {
+  PROJECT_TECHNOLOGIES,
+  PROJECT_TECHNOLOGY_CATEGORY,
+} from "../../../constants/project-techologies";
+import { IProject } from "../../../interfaces/project";
+import { ProjectExplorerList } from "@/components/project/ExplorerList";
+import { getTranslations, unstable_setRequestLocale } from "next-intl/server";
+import { useTranslations } from "next-intl";
+import { DEFAULT_LOCALE } from "@/constants/locales";
+import { Link } from "@/navigation";
 import { Metadata } from "next";
-import { getDictionary } from "@/utils/get-dictionary";
-import { Locale } from "@/i18n-config";
+import { ProjectListEmpty } from "@/components/project/ListEmpty";
 
-interface Props {
-  params: {
-    projectCode: string;
-    locale: Locale;
-  };
-}
+export const getDataByCategory = (lang:string, category?: string) => {
+  return (
+    category
+      ? [...PROJECTS.filter((project) => project.categoryCode === category)]
+      : [...PROJECTS]
+  )
+    .map<IProject>((project) => ({
+      ...project,
+      technologies: PROJECT_TECHNOLOGIES.filter((technology) =>
+        project.technologyCodes.includes(technology.code),
+      ).map((technology) => ({
+        ...technology,
+        categories: PROJECT_TECHNOLOGY_CATEGORY.filter((category) =>
+          technology.categoryCodes.includes(category.value) && project.lang === lang,
+        ),
+      })),
+    }))
+    .sort((a, b) => {
+      if (a.priority && b.priority) {
+        return b.priority - a.priority;
+      }
+      if (a.start && b.start) {
+        return new Date(b.start).getTime() - new Date(a.start).getTime();
+      }
+      return 0;
+    });
+};
 
-export async function generateMetadata({
-  params,
-}: Props): Promise<Metadata | undefined> {
-  const { projectCode, locale } = params;
-  const dict = await getDictionary(locale);
+type Props = {
+  params: Promise<{ category?: string; locale?: string }>;
+};
 
-  const project = PROJECTS.find(
-    (project) => project.code === projectCode && project.lang === locale
-  );
-
-  if (!project) {
-    return;
-  }
-
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const { locale = DEFAULT_LOCALE } = await params;
+  const t = await getTranslations({ locale, namespace: "Projects" });
   return {
-    title: {
-      default: project.title,
-      template: `%s | ${dict.Metadata.title.template}`,
-    },
-    description: project.shortDescription,
+    title: t("title"),
   };
 }
 
-export default function ProjectPage({ params }: Props) {
-  const { projectCode, locale } = params;
+export default function ProjectsPage({
+  params
+}: Props) {
+  const { locale = DEFAULT_LOCALE, category }  = use(params)
+  unstable_setRequestLocale(locale);
+  const categories = PROJECT_CATEGORIES;
+  const initialProjects = getDataByCategory(category, locale);
+  const t = useTranslations("Projects");
 
-  const project = PROJECTS.find(
-    (project) => project.code === projectCode && project.lang === locale
+  return (
+    <section
+      data-testid="projects"
+      className="container flex flex-col items-center justify-center gap-4"
+    >
+      <h1 className="font-monospace flex flex-row items-center gap-4 text-3xl dark:text-white lg:text-4xl">
+        <CodeBracketIcon className="h-8 w-8" />
+        {t("title")}
+      </h1>
+      <div className="mx-10 flex w-full flex-row flex-wrap items-center justify-center gap-2">
+        <Link
+          className={`link ${
+            !category
+              ? "bg-slate-500 text-white"
+              : "text-black hover:text-slate-500 dark:text-white dark:hover:text-slate-500"
+          } rounded px-2 py-1`}
+          aria-label={`link to all projects`}
+          href={`/projects`}
+          locale={locale}
+        >
+          {t("categories.all")}
+        </Link>
+        {categories.map((item) => (
+          <Link
+            key={item.value}
+            className={`link ${
+              category === item.value
+                ? "bg-slate-500 text-white"
+                : "text-black hover:text-slate-500 dark:text-white dark:hover:text-slate-500"
+            } rounded px-2 py-1`}
+            aria-label={`link to ${item.name} projects`}
+            href={`/projects/c/${item.value}`}
+            locale={locale}
+          >
+            {item.name}
+          </Link>
+        ))}
+      </div>
+      {initialProjects.length ? (
+        <ProjectExplorerList projects={initialProjects} />
+      ) : (
+        <ProjectListEmpty />
+      )}
+    </section>
   );
-
-  if (!project) {
-    notFound();
-  }
-
-  return <Project project={project as IBaseProject} />;
 }
