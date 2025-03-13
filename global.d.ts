ac7d495b95a72c12d5359f23ddea09466a2ac9e7
Franz Bendezu <63468778+franz-bendezu@users.noreply.github.com>
2025-03-13 06:08:38 +0000

    refactor(i18n): update Messages type in AppConfig to support multiple language imports

--- global.d.ts
+++ global.d.ts
@@ -1,10 +1,11 @@
 // global.d.ts
-import { routing } from "./i18n/routing";
-import messages from '@/messages/en.json';
+import type { routing } from "./i18n/routing";
+import type messagesEn from "@/messages/en.json";
+import type messagesEs from "@/messages/es.json";
 
 declare module "next-intl" {
   interface AppConfig {
-    Locale: (typeof routing.locales)[number]; 
-    Messages: typeof messages;
+    Locale: (typeof routing.locales)[number];
+    Messages: typeof messagesEn | typeof messagesEs;
   }
 }
