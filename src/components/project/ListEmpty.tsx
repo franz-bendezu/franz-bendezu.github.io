13c6b546f2eb21b729db8feed107573230400871
Franz Bendezu <63468778+franz-bendezu@users.noreply.github.com>
2026-07-29 04:32:36 +0000

    feat: migrate portfolio to Astro

--- src/components/project/ListEmpty.tsx
+++ src/components/project/ListEmpty.tsx
@@ -1,9 +1,10 @@
 import { ExclamationTriangleIcon } from "@heroicons/react/16/solid";
-import { FC } from "react";
 
-export const ProjectListEmpty: FC = () => (
-  <div className="flex w-full flex-col items-center justify-center gap-4">
-    <ExclamationTriangleIcon className="h-12 w-12 text-yellow-500 dark:text-yellow-500" />
-    <p className="text-2xl dark:text-white">No hay proyectos para mostrar</p>
-  </div>
-);
+export function ProjectListEmpty({ label }: { label: string }) {
+  return (
+    <div className="flex w-full flex-col items-center justify-center gap-4 py-16">
+      <ExclamationTriangleIcon className="h-12 w-12 text-yellow-500" />
+      <p className="text-2xl dark:text-white">{label}</p>
+    </div>
+  );
+}
